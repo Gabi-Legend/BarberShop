4660efedf0f8ec019d1d8267bd8b4c60275cb48e
Gabriel Mazuru <gabrielmazuru6@gmail.com>
2025-08-11 15:02:38 +0300

    Contact form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -5,6 +5,23 @@ export default function Contact() {
   return (
     <>
       <NavBar />
+      <div className={styles.contactContainer}>
+        <div className={styles.contactCard}>
+          <h1 className={styles.contactTitle}>Pagina de Contact</h1>
+
+          <div>
+            <label className={styles.contactLabel}>Email</label>
+            <input type="email" className={styles.contactInput} />
+          </div>
+
+          <div>
+            <label className={styles.contactLabel}>Mesaj</label>
+            <textarea rows={4} className={styles.contactTextarea}></textarea>
+          </div>
+
+          <button className={styles.trimite}>Trimite</button>
+        </div>
+      </div>
     </>
   );
 }
